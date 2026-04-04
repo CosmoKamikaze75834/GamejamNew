f8c1ade6dcadbfee8d2ac7bb1b7224f40973a08c
CosmoKamikaze75834 <kosmogrezz.05@gmail.com>
2026-04-04 13:00:51 +0300

    Добавила стрельбу

--- Assets/_Project/Scripts/Runtime/GamePlay/Player/Shooting.cs
+++ Assets/_Project/Scripts/Runtime/GamePlay/Player/Shooting.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 public class Shooting: MonoBehaviour
@@ -6,12 +6,21 @@ public class Shooting: MonoBehaviour
     [SerializeField] private Transform _firePoint;
     [SerializeField] private Bullet _bulletPrefab;
     [SerializeField] private float _bulletForce = 20f;
+    [SerializeField] private float _delay = 3f;
+
+    private bool _canShoot = true;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Shoot();
+            if (_canShoot == true)
+            {
+                Debug.Log("Shoot");
+                Shoot();
+                _canShoot = false;
+                StartCoroutine(TimeShoot());
+            }
         }
     }
 
@@ -21,4 +30,15 @@ public class Shooting: MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(_firePoint.up * _bulletForce, ForceMode2D.Impulse);
     }
+
+    private IEnumerator TimeShoot()
+    {
+        yield return new WaitForSeconds(_delay);
+        _canShoot = true;
+    }
+}
+
+public class MovingPerson : MonoBehaviour
+{
+
 }
