796aa9887adc81305fe0f849e8aefcce86365a51
CosmoKamikaze75834 <kosmogrezz.05@gmail.com>
2026-04-04 14:22:03 +0300

    Следование объектов за игроком

--- Assets/_Project/Scripts/Runtime/GamePlay/Player/Bullet.cs
+++ Assets/_Project/Scripts/Runtime/GamePlay/Player/Bullet.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Threading;
 using UnityEngine;
 
 public class Bullet: MonoBehaviour
 {
+    public event Action<Person> BulletAchieved;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.TryGetComponent<Enemy>(out _))
+        if(collision.TryGetComponent(out Person person))
         {
+            BulletAchieved?.Invoke(person);
+            person.StartChasing();
             Destroy(gameObject);
         }
     }
