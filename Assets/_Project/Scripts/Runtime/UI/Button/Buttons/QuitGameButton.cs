c38eb53cb6545ffbb6e077f75eeaf4181ff01468
ValeryLangolf <lvv.89@rambler.ru>
2026-04-04 14:22:14 +0300

    Переработан режим скрытия PopUp

--- Assets/_Project/Scripts/Runtime/UI/Button/Buttons/QuitGameButton.cs
+++ Assets/_Project/Scripts/Runtime/UI/Button/Buttons/QuitGameButton.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 public class QuitGameButton : ButtonClickHandler
 {
     [SerializeField] private PopUp _quitGameDialog;
-    [SerializeField] private PopUp _buttonsVerticalLayout;
 
-    public override void OnClick()
-    {
+    public override void OnClick() =>
         _quitGameDialog.Show();
-        _buttonsVerticalLayout.Hide();
-    }
 }
--- Assets/_Project/Scripts/Runtime/UI/Button/Buttons/SettingsCloserButton.cs
+++ Assets/_Project/Scripts/Runtime/UI/Button/Buttons/SettingsCloserButton.cs
@@ -2,14 +2,8 @@ using UnityEngine;
 
 public class SettingsCloserButton : ButtonClickHandler
 {
-    [SerializeField] private PopUp _popUp;
-    [SerializeField] private PopUp _buttonsVerticalLayout;
+    [SerializeField] private PopUp _menuButtons;
 
-    public override void OnClick()
-    {
-        if(_buttonsVerticalLayout != null)
-            _buttonsVerticalLayout.Show();
-
-        _popUp.Hide();
-    }
+    public override void OnClick() =>
+        _menuButtons.Show();
 }
