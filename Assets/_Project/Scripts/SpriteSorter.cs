448bcd442fc580b4a21453b1b87f59bb511f6c8d
CosmoKamikaze75834 <kosmogrezz.05@gmail.com>
2026-04-05 19:43:28 +0300

    Добавила логику передвижения по кустам

--- Assets/_Project/Scripts/SpriteSorter.cs
+++ Assets/_Project/Scripts/SpriteSorter.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SpriteSorter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _offset = 0.4f;
+    private int _sortingOrderBase = 0;
+    private SpriteRenderer _renderer;
+
+    private bool _isStatic = false;
+
+    private void Awake()
     {
-        
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        _renderer.sortingOrder = (int)(_sortingOrderBase - transform.position.y + _offset);
+
+        if(_isStatic)
+        {
+            Destroy(this);
+        }
     }
 }
